b96d2204763bdcadf79a2ec90f9b9d0c97bf6912
LizbethFP <lizbeth.fp@gmail.com>
2018-03-15 00:44:42 -0500

    probando lista de hobbies

--- src/components/HobbieList.js
+++ src/components/HobbieList.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import HobbieListItem from './HobbieListItem';
+
+
+const hobbiesArray = ['comer', 'ir al cine', 'asistir a un concierto', 'leer cómics'];
+
+const HobbieList = () => (
+  <ul>
+    <HobbieListItem hobbiesprop = {hobbiesArray}/>
+  </ul>
+)
+
+export default HobbieList;
--- src/components/HobbieListItem.js
+++ src/components/HobbieListItem.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const HobbieListItem = ({hobbiesArray}) => (
+  <div>
+    {hobbiesArray.map(el => (<li key={el}>{el}</li>))}
+  </div>
+)
+
+HobbieListItem.PropTypes = {
+  hobbiesArray: PropTypes.arrayOf(PropTypes.string);
+}
+
+export default HobbieListItem;

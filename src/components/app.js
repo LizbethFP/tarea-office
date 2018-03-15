18c54d03d5570a1df7b626f8ad3f9c218ce93c69
LizbethFP <lizbeth.fp@gmail.com>
2018-03-15 00:45:53 -0500

    comentando hobbieList

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import Header from './Header'
 import Section from './Section'
-import HobbieList from './HobbieList';
+// import HobbieList from './HobbieList';
 
 const App = () => (
   <div>
     <Header/>
-    <HobbieList/>
     <Section/>
   </div>
   )
